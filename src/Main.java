4b3da7d54e4437fc1695ca009bd4f3f5d7132bfc
Carol He <ch2727@nyu.edu>
2019-04-24 23:32:32 -0400

    do they cross working with tests

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,8 @@ public static void main(String[] args){
 	}
 	/////////
 
-
+  System.out.println("Do They Cross: " + doTheyCross(A,B,C,D));
+  System.out.println("Do They Cross: " + doTheyCross(A,C,B,D));
 
 	Point temp;
 	while(ordered.size() < points.size()){
@@ -57,8 +58,6 @@ public static void main(String[] args){
 		removePoint(temp); //remove temp from all distance tracker lists
 
 	}
-
-  System.out.println("Do They Cross: " + doTheyCross(A,B,C,D));
 
 	outputResults();
 
@@ -223,27 +222,74 @@ public static void addPointToOrdered(Point a){
 
 
 
-public static int orientation(Point p, Point q, Point r) {
-    double val = (q.getY() - p.getY()) * (r.getX() - q.getX())
-            - (q.getX() - p.getX()) * (r.getY() - q.getY());
+// Given three colinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+public static boolean onSegment(Point p, Point q, Point r)
+{
+    if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+        q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+       return true;
 
-    if (val == 0.0)
-        return 0; // colinear
-    return (val > 0) ? 1 : 2; // clock or counterclock wise
+    return false;
 }
 
-public static boolean doTheyCross(Point startA, Point endA, Point startB, Point endB){
-  int o1 = orientation(startA, startB, endA);
-  int o2 = orientation(startA, startB, endB);
-  int o3 = orientation(endA, endB, startA);
-  int o4 = orientation(endA, endB, startB);
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+public static int orientation(Point p, Point q, Point r)
+{
+    int val = (q.y - p.y) * (r.x - q.x) -
+              (q.x - p.x) * (r.y - q.y);
 
-  if (o1 != o2 && o3 != o4)
-      return true;
+    if (val == 0) return 0;  // colinear
 
-  return false;
-
+    return (val > 0)? 1: 2; // clock or counterclock wise
 }
+
+// referenc: https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+public static boolean doTheyCross(Point p1, Point q1, Point p2, Point q2)
+{ //p1q1 and p2q2 are line segments
+    // Find the four orientations needed for general and
+    // special cases
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    // Special Cases
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+    // p1, q1 and q2 are colinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+     // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+    return false; // Doesn't fall in any of the above cases
+}
+
+
+// // Given three colinear points p, q, r, the function checks if
+// // point q lies on line segment 'pr'
+// public static boolean onSegment(Point p, Point q, Point r)
+// {
+//   System.out.println("TEST");
+//     if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+//         q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+//        return true;
+//
+//     return false;
+// }
 
 
 //output results in a nice and easy to read way
